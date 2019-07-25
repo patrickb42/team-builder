f6d9fbe48c888eecab990e4efcd4525e73f80af2
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-07-24 21:13:10 -0400

    teamMemberFormData updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
+import Form from './components/Form';
+
 const App: React.FC = () => {
   const [placeholdState, setPlaceholderState] = useState<TeamMemberTS[]>();
 
   return (
     <Router>
-      {/* <Navigation /> */}
-      <h1>test</h1>
+      <Form />
     </Router>
   );
 };
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,2 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const submitMember = (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+};
+
+
+interface FormProps {
+  membersList: TeamMemberTS[] | undefined;
+  setMembersList: React.Dispatch<React.SetStateAction<TeamMemberTS[]>> | undefined;
+}
+const Form: React.FC = () => {
+  const [teamMemberFormData, setTeamMemberFormData] = useState<TeamMemberTS | undefined>(undefined);
+
+  const updateTeamMemberData = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTeamMemberFormData({
+      ...teamMemberFormData,
+      [e.currentTarget.name]: e.currentTarget.value
+    } as TeamMemberTS);
+  };
+
+  return (
+    <form onSubmit={submitMember}>
+      Name:&nbsp;
+      <input
+        name="name"
+        type="text"
+        placeholder="Taylor Smith"
+        onChange={updateTeamMemberData}
+        /><br/>
+      Email:&nbsp;
+      <input
+        name="email"
+        type="text"
+        placeholder="TaylorSmith@gmail.com"
+        onChange={updateTeamMemberData}
+        /><br/>
+      Role:&nbsp;
+      <input name="role"
+        type="text"
+        placeholder="Software Developer"
+        onChange={updateTeamMemberData}
+      /><br/>
+      <button type="submit">submit</button>
+    </form>
+  );
+};
+
+export default Form;
